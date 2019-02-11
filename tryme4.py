b929250bbf079c32ad296c8a6cbaba1533fae139
Keeno Simms <46448798+keenosimms@users.noreply.github.com>
2019-02-10 20:36:27 -0500

    Add files via upload

--- tryme4.py
+++ tryme4.py
@@ -1,26 +1,28 @@
-def new_line(): 
+def new_line(): #defined one line, new_line
     print('.')
 
-def three_lines():
-    new_line()
+def three_lines():#defined three_lines
+    new_line()    #nested function calls
     new_line()
     new_line()
 
-def nine_lines(): #defined nine lines using function three lines 
-    three_lines() #nested function
+def nine_lines(): #defined nine_lines 
+    three_lines() #nested function calls
     three_lines()
     three_lines()
 
-def clear_screen(): #defined clear_lines using functions nine_lines, three_lines
-    nine_lines()    #nested function 
+def clear_screen(): #defined clear_screen will print 25 lines
+    nine_lines()    #nested function calls 
     nine_lines()
     three_lines()
     three_lines()
     new_line()
-    
-clear_screen()
 
-#by Keeno Simms
-# Saturday February 9th, 2019 7:07pm
+print("Program Will Now Print 25 Lines") 
+    
+clear_screen() #“Calling clearScreen()”.
+
+#completed by Keeno Simms
+#Saturday February 10th, 2019 8:27pm
 
     
